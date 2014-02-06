89885354677c2279370b5b1dff1fdc5c8119fb77
Agam Rafaeli <agam.rafaeli@gmail.com>
2014-02-06 19:16:42 +0200

    get forms from inner pages

--- content.js
+++ content.js
@@ -1,12 +1,23 @@
 $(document).ready(function ()
 {
-	$('a').each(function(index,value) {
-		alert(value.href)
-	})
+	links = $("a[href]")
+	$("body").html("")
 
+	.each(function(index,value) {
+	$.ajax({
+			url: value.href,
+			dataType: "html",
+			success: function(data) {
+				$($(data)).find("form[action='https://www.paypal.com/cgi-bin/webscr']").each(function() {
+					var inputs = $(this).find('input');
+					$(this).empty().append(inputs);
+					$("body").append($(this).html());
+				});
+			}
+		});
+	})
 });
 
-
-function parseLinks(pageContent) {
+function parseOldForm(oldForm) {
 
 }
